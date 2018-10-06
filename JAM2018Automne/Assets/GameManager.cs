a0497e16affd009e4518e9ac557039761886049d
Kitolex <kitolex.soluce@gmail.com>
2018-10-06 14:22:20 -0400

    vote manager + effect manager

--- JAM2018Automne/Assets/GameManager.cs
+++ JAM2018Automne/Assets/GameManager.cs
@@ -2,47 +2,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = System.Random;
 
 public class GameManager : MonoBehaviour {
 
     public float timerChrono;
     public float timerVote;
     private float time;
-    private List<Effect> listEffectEnCours;
+   
     private EtatGame etat;
+    private VoteManager voteManager;
+    private EffectManager effectManager;
 
     public Vote v;
 
     // Use this for initialization
     void Start () {
-
-        for(int i=0;i<v.nomProposition.Count; i++)
-        {
-            Debug.Log(v.nomProposition[i]);
-            Debug.Log(v.listEffects[i].effects.Count);
-            for (int j = 0; j < v.listEffects[i].effects.Count; j++)
-            {
-
-                Debug.Log(v.listEffects[i].effects[j].ToString());
-
-            }
-        }
-
+        voteManager = GameObject.FindGameObjectWithTag("VoteManager").GetComponent<VoteManager>();
+        effectManager = GameObject.FindGameObjectWithTag("EffectManager").GetComponent<EffectManager>();
         etat = EtatGame.bataille;
         time = Time.time;
-        listEffectEnCours = new List<Effect>();
+        
     }
 	
 	// Update is called once per frame
 	void Update () {
+
         if (etat.Equals(EtatGame.bataille))
         {
-            DisplayEffects();
+            effectManager.DisplayEffects();
             if (timerChrono <= (Time.time - time))
             {
                 time = Time.time;
                 etat = EtatGame.vote;
-                StartVote();
+                voteManager.startVote();
             }
         }
         if (etat.Equals(EtatGame.vote))
@@ -50,9 +43,9 @@ public class GameManager : MonoBehaviour {
             if (timerVote <= (Time.time - time))
             {
                 time = Time.time;
-                EndEffects();               
+                effectManager.EndEffects();               
                 StopVote();
-                BeginEffects();
+                effectManager.BeginEffects();
                 etat = EtatGame.bataille;
             }
         }
@@ -60,38 +53,16 @@ public class GameManager : MonoBehaviour {
 
     }
 
-    private void DisplayEffects()
-    {
-        foreach (Effect e in listEffectEnCours)
-        {
-            e.Display();
-        }
-    }
-    private void EndEffects()
-    {
-        foreach (Effect e in listEffectEnCours)
-        {
-            e.End();
-        }
-    }
-    private void BeginEffects()
-    {
-        foreach (Effect e in listEffectEnCours)
-        {
-            e.Begin();
-        }
-    }
+    
 
     private void StopVote()
     {
-        Debug.Log("Fin Vote");
+        List<ListEffet> listEffect = new List<ListEffet>();
+        listEffect.AddRange(voteManager.getEffect());
+        effectManager.createliste(listEffect);
     }
 
-    private void StartVote()
-    {
-        Debug.Log("Debut Vote");
-        
-    }
+
     
 }
 
