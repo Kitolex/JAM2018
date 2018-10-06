8f2f5d816b5f5f58928fb25235ef45dbe5cf118e
Kitolex <kitolex.soluce@gmail.com>
2018-10-05 23:41:49 -0400

    debut gamemanger

---
+++ JAM2018Automne/Assets/Effect.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Effect : MonoBehaviour {
+
+    public abstract void Begin();
+
+    public abstract void Display();
+
+    public abstract void End();
+
+
+}
---
+++ JAM2018Automne/Assets/GameManager.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public float timerChrono;
+    public float timerVote;
+    private float time;
+    private List<Effect> listEffectEnCours;
+    private EtatGame etat;
+
+    // Use this for initialization
+    void Start () {
+        etat = EtatGame.bataille;
+        time = Time.time;
+        listEffectEnCours = new List<Effect>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (etat.Equals(EtatGame.bataille))
+        {
+            foreach (Effect e in listEffectEnCours)
+            {
+                e.Display();
+            }           
+
+            if (timerChrono <= (Time.time - time))
+            {
+                time = Time.time;
+                StartVote();
+            }
+        }
+        if (etat.Equals(EtatGame.vote))
+        {
+            if (timerChrono <= (Time.time - time))
+            {
+                time = Time.time;
+                StopVote();
+            }
+        }
+
+
+    }
+
+    private void StopVote()
+    {
+        Debug.Log("Fin Vote");
+        etat = EtatGame.bataille;
+    }
+
+    private void StartVote()
+    {
+        Debug.Log("Debut Vote");
+        etat = EtatGame.vote;
+    }
+
+    public void AddEffect()
+    {
+
+    }
+
+    
+}
+
+public enum EtatGame
+{
+    preparation,
+    vote,
+    bataille,
+}
