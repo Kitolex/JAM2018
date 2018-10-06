901ef816cbbd6558f5af35a89ba553c7f29c6285
Unknown <leo.posticblu@orange.fr>
2018-10-06 08:48:02 -0400

    Init

---
+++ JAM2018Automne/Assets/Scripts/MultiplayerManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MultiplayerManager : MonoBehaviour {
+
+    public int numberPlayer;
+    public int[] Scores;
+    public GameObject[] playerList;
+    public Vector3[] SpawnPoints;
+
+    public void InstanciatePlayer(int numPlayer)
+    {
+        GameObject instanciated;
+
+        //si le joueur n'existe pas 
+        if (numPlayer > numberPlayer)
+            return;
+
+        if (playerList[numPlayer])
+        {
+            instanciated = Instantiate(playerList[numPlayer]);
+            instanciated.transform.position = SpawnPoints[numPlayer];
+        }           
+    }
+}
