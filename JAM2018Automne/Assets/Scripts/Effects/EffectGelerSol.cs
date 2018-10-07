2825276152c6c520db1372f40aaebfe09bb7ee4f
Brandon <brandon.piard@etu.univ-lyon1.fr>
2018-10-07 13:34:35 -0400

    Commit post merge

--- JAM2018Automne/Assets/Scripts/Effects/EffectGelerSol.cs
+++ JAM2018Automne/Assets/Scripts/Effects/EffectGelerSol.cs
@@ -4,12 +4,15 @@ using UnityEngine;
 
 public class EffectGelerSol : Effect
 {
+
+
     public override void Begin() {
         foreach(PersonnageBehaviour pb in this.multiplayerManager.GetAllPersonnages()){
 
             pb.solGlace = true;
 		}
         changeDecorsMaterials("LowPoly_Ice1");
+        
     }
 
     public override void Display() {
@@ -24,48 +27,51 @@ public class EffectGelerSol : Effect
         changeDecorsMaterials("LowPoly_Rock");
     }
 
+
+
+
     private void changeDecorsMaterials(string materialName)
     {
         Material iceMaterial = getMaterial(materialName);
         GameObject decors = GameObject.FindGameObjectWithTag("mutableMaterials");
         for( int i=0; i< decors.transform.childCount; i++)
         {
-            Renderer r = decors.transform.GetChild(i).GetComponent<Renderer>();
-            if (r)
+            Renderer[] renderers = decors.transform.GetChild(i).GetComponents<Renderer>();
+            if (renderers.Length > 0)
             {
-                r.sharedMaterial = iceMaterial;
+                foreach (Renderer r in renderers)
+                { 
+                    for (int j=0; j<r.materials.Length; j++)
+                    {
+                        Debug.Log("actual material : " + r.sharedMaterials[j].name);
+                        Debug.Log("ice material : " + iceMaterial.name);
+                        r.materials[j] = iceMaterial;
+                        r.sharedMaterials[j] = iceMaterial;
+                    }
+                }
             }
         }
     }
 
     private void changeMapMaterials(string materialName)
     {
-        Material iceGroundMaterial = getMaterial(materialName);
-        GameObject decors = GameObject.FindGameObjectWithTag("mutableMaterials");
-        for (int i = 0; i < decors.transform.childCount; i++)
-        {
-            Renderer r = decors.transform.GetChild(i).GetComponent<Renderer>();
-            if (r)
-            {
-                r.sharedMaterial = iceGroundMaterial;
-            }
-        }
+        
     }
 
     private Material getMaterial(string materialName)
     {
-        GameObject[] materials = GameObject.FindGameObjectsWithTag("material");
+        GameObject[] materialsObject = GameObject.FindGameObjectsWithTag("material");
         Material material = null;
-        foreach (GameObject go in materials)
+        foreach (GameObject go in materialsObject)
         {
-            if (go.GetComponent<Renderer>().material.name.Equals(materialName))
-            {
-                material = go.GetComponent<Renderer>().material;
+            if (go.GetComponent<Renderer>().sharedMaterial.name.Equals(materialName))
+            { 
+                material = go.GetComponent<Renderer>().sharedMaterial;
             }
         }
         if (!material)
         {
-            Debug.Log("Ground material not found");
+            return null;
         }
         return material;
     }
