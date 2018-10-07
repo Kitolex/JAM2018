b98f8a070fafa5cb092b4ca89d96c89a9d509cf8
Fiesta87 <jb@pommeret.fr>
2018-10-07 13:28:57 -0400

    affect perdre vie + effect walls + ajustement pillars

---
+++ JAM2018Automne/Assets/Scripts/Effects/EffectPerdreVie.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EffectPerdreVie : Effect
+{
+    public override void Begin() {
+        foreach(PersonnageBehaviour pb in this.multiplayerManager.GetAllPersonnages()){
+
+            pb.stun(0.8f);
+			pb.Kill();
+            if (pb.IsDead()){
+                pb.gameObject.SetActive(false);
+            }
+		}
+    }
+
+    public override void Display() {
+        
+    }
+
+    public override void End() {
+        
+    }
+}
---
+++ JAM2018Automne/Assets/Scripts/Effects/EffectWalls.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EffectWalls : Effect
+{
+
+    private int nbObjectToAdd = 2;
+    private GameObject[] objects;
+
+    public override void Begin() {
+
+        objects = new GameObject[nbObjectToAdd];
+
+        int nbPrefabs = effectManager.walls.Length;
+
+        float maxRayon = mapManager.getDiametreMap() * 0.5f - 1.0f;
+
+        Vector3 posi = Vector3.up * 40.0f;
+
+        for(int i=0; i<nbObjectToAdd; i++){
+            Transform obj = effectManager.walls[Random.Range(0, nbPrefabs)];
+            objects[i] = Instantiate(obj, posi + Vector3.right * Random.Range(0.0f, maxRayon), obj.rotation).gameObject;
+        }
+    }
+
+    public override void Display() {
+        
+    }
+
+    public override void End() {
+        for(int i=0; i<nbObjectToAdd; i++) {
+            Destroy(objects[i]);
+        }
+    }
+}
