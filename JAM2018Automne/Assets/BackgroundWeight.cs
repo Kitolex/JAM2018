a87962d7c2ff1537422c122ceaaa70607557bde9
Julie <juliemounier96@gmail.com>
2018-10-06 21:09:05 -0400

    Starting with Displaying Player Info

---
+++ JAM2018Automne/Assets/BackgroundWeight.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundWeight : MonoBehaviour {
+
+    public float animWeight = 0.5f;
+    private Animator anim;
+
+    // Use this for initialization
+    void Start () {
+        anim.SetLayerWeight(10, animWeight);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        anim.SetLayerWeight(anim.GetLayerIndex("BG"), animWeight);
+    }
+}
