01d6200f20f354c2b3763a1d9d41cf663848ab98
Unknown <leo.posticblu@orange.fr>
2018-10-19 15:58:19 -0400

    Fix piliers

--- JAM2018Automne/Assets/Scripts/FallOnStart.cs
+++ JAM2018Automne/Assets/Scripts/FallOnStart.cs
@@ -5,31 +5,30 @@ using UnityEngine;
 public class FallOnStart : MonoBehaviour {
 
 	public Transform poussiereImpactSol;
+    public float Height = 10;
+    public float FallSpeed = 5;
 	private bool effectDone;
 
 	// Use this for initialization
 	void Start () {
 		effectDone = false;
-		this.GetComponent<Rigidbody>().velocity = Vector3.up * -60.0f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        if (this.GetComponent<Rigidbody>().velocity.y >= 0)
-            this.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
+        if (transform.position.y > Height)
+            transform.position -= Vector3.down * FallSpeed * Time.deltaTime;
+        else
+        {
+            if (!effectDone)
+            {
+                effectDone = true;
+
+                Vector3 posi = this.transform.position;
+                posi.y = 0.4f;
+                Instantiate(poussiereImpactSol, posi, poussiereImpactSol.rotation);
+            }
+        }
 
     }
-
-	void OnCollisionEnter(Collision collision) {
-		
-		if(!effectDone){
-			effectDone = true;
-
-			Vector3 posi = this.transform.position;
-			posi.y = 0.4f;
-			Instantiate(poussiereImpactSol, posi, poussiereImpactSol.rotation);
-		}
-		
-		
-	}
 }
